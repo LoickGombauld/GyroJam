eff4dadf31c1e03c9c718ca8a325c121f6316025
loickGombauld <wild1907@hotmail.fr>
2023-09-07 10:32:11 +0200

    fixed BlackHoleData

--- Assets/Scripts/Loïck/BlackholeData.cs
+++ Assets/Scripts/Loïck/BlackholeData.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlackholeData : MonoBehaviour
+public class BlackholeData : MinigameBaseData
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int NumEnemytoKill = 3;
 }
