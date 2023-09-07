0131bf2e71b4930c3f5c6730de705480e8573472
loickGombauld <wild1907@hotmail.fr>
2023-09-07 14:56:08 +0200

    Update Player

--- Assets/Scripts/Loïck/Player.cs
+++ Assets/Scripts/Loïck/Player.cs
@@ -2,27 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private Rigidbody2D _rb;
-    [SerializeField] private Vector2 _dir;
-    [SerializeField] private float _playerRadius;
-    public float PlayerRadius => _playerRadius;
+    [SerializeField] private Vector2 _dir; 
+    private CircleCollider2D _collider;
+    public float PlayerRadius => _collider.radius;
 
     private Vector2 _vel;
-    private  Gyro _playerInput = new Gyro();
-    private bool _canMove = true;
 
-    private void Start()
-    {
-        _playerInput.Enable();
-    }
+    private bool _canMove = true;
 
     public void MovePlayer()
     {
@@ -30,46 +27,24 @@ public class Player : MonoBehaviour
         _rb.MovePosition(_vel * Time.fixedDeltaTime);
     }
 
+    private void Start() => _collider = GetComponent<CircleCollider2D>() ?? transform.AddComponent<CircleCollider2D>();
 
     private void Update()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         if (_canMove)
         {
             MovePlayer();
         }
     }
-
-    private void RegisterInputs()
-    {
-        Gyro.ControlsActions l_playerAction = _playerInput.Controls;
-        l_playerAction.TrackBall.performed += GetDirectionMovement;
-        l_playerAction.TrackBall.canceled += CancelDirectionMovement;
-    }
-
-    private void UnregisterInputs()
-    {
-        Gyro.ControlsActions l_playerAction = _playerInput.Controls;
-        l_playerAction.TrackBall.performed -= GetDirectionMovement;
-        l_playerAction.TrackBall.canceled -= CancelDirectionMovement;
-    }
-
-    private void OnEnable()
-    {
-        RegisterInputs();
-    }
-
-    private void OnDisable()
-    {
-        UnregisterInputs();
-    }
-
-    private void GetDirectionMovement(InputAction.CallbackContext p_ctx)
+    
+    public void GetDirectionMovement(InputAction.CallbackContext p_ctx)
     {
         Vector2 l_direction = p_ctx.ReadValue<Vector2>();
         _dir = l_direction;
     }
 
-    private void CancelDirectionMovement(InputAction.CallbackContext p_ctx)
+    public void CancelDirectionMovement(InputAction.CallbackContext p_ctx)
     {
         _dir = Vector2.zero;
     }
